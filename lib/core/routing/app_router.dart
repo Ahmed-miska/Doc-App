02c7fb159bde2091aaf510f319641e1562e55e6d
Ahmed-miska <miskaa004@gmail.com>
2024-06-23 06:46:26 +0300

    -crate login logic
    Signed-off-by: Ahmed-miska <miskaa004@gmail.com>

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_complete_project/core/di/dependency_injection.dart';
 import 'package:flutter_complete_project/core/routing/routes.dart';
+import 'package:flutter_complete_project/features/login/logic/cubit/login_cubit.dart';
 import 'package:flutter_complete_project/features/login/ui/login_screen.dart';
 import 'package:flutter_complete_project/features/onboarding/onboarding_screen.dart';
 
@@ -14,7 +17,10 @@ class AppRouter {
         );
       case Routes.loginScreen:
         return MaterialPageRoute(
-          builder: (_) => const LoginScreen(),
+          builder: (_) => BlocProvider(
+            create: (context) => getit<LoginCubit>(),
+            child: const LoginScreen(),
+          ),
         );
       default:
         return MaterialPageRoute(
