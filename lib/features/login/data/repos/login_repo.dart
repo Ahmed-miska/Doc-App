84d5dc5b7a2ea726f900454895958620605cc197
Ahmed-miska <miskaa004@gmail.com>
2024-06-24 09:09:04 +0300

    - fix issuse

--- lib/features/login/data/repos/login_repo.dart
+++ lib/features/login/data/repos/login_repo.dart
@@ -1,10 +1,7 @@
 import 'package:flutter_complete_project/core/networking/api_error_handler.dart';
 import 'package:flutter_complete_project/core/networking/api_result.dart';
-<<<<<<< HEAD
-import 'package:flutter_complete_project/core/networking/api_service.dart';
-=======
+
 import 'package:flutter_complete_project/core/networking/api_services.dart';
->>>>>>> f02af74c77bc7683e9262a9d3a35e05ee136ee46
 import 'package:flutter_complete_project/features/login/data/models/login_request_body.dart';
 import 'package:flutter_complete_project/features/login/data/models/login_response.dart';
 
@@ -17,18 +14,11 @@ class LoginRepo {
     try {
       final response = await _apiService.login(loginRequestBody);
       return ApiResult.success(response);
-<<<<<<< HEAD
-    } catch (error) {
-      return ApiResult.failure( ErrorHandler.handle(error));
-    }
-  }
-  
-=======
+
     } catch (errro) {
       return ApiResult.failure(ErrorHandler.handle(errro));
     }
   }
->>>>>>> f02af74c77bc7683e9262a9d3a35e05ee136ee46
 }
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,9 @@ import 'package:flutter_complete_project/doc_app.dart';
 import 'core/routing/app_router.dart';
 
 void main() {
-<<<<<<< HEAD
-  setupGetIt();
-=======
+
    setupGetIt();
->>>>>>> f02af74c77bc7683e9262a9d3a35e05ee136ee46
+
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
